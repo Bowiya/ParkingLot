61394d3aa1e48663cb83de5d43d41819fec42b54
Bowiya S <bowiya.s@thoughtworks.com>
2022-08-10 12:36:57 +0530

    Sathya,Bowiya,Jayaprakash: used four constant string messages to replace the return statement in parkingLot class

--- src/main/java/com/tw/parkinglot/ParkingLot.java
+++ src/main/java/com/tw/parkinglot/ParkingLot.java
@@ -4,6 +4,10 @@ import java.util.ArrayList;
 
 public class ParkingLot {
     public final int capacity;
+    public final String PARKED = "Your car is parked";
+    public final String NOT_PARKED = "Your car is not parked";
+    public final String UNPARKED = "Your car is unparked";
+    public final String CAR_TO_BE_UNPARKED_IS_NOT_PRESENT = "The car you are asking to unpark is not present";
 
     ArrayList<Object> parkedCars = new ArrayList<>();
 
@@ -14,16 +18,16 @@ public class ParkingLot {
     public String park(Object car) {
         if (parkedCars.size() < capacity) {
             parkedCars.add(car);
-            return "Your car is parked";
+            return PARKED;
         }
-        return "Your car is not parked";
+        return NOT_PARKED;
     }
 
     public String unPark(Object car) {
         if (parkedCars.contains(car)) {
             parkedCars.remove(car);
-            return "Your car is unparked";
+            return UNPARKED;
         }
-        return "The car you are asking to unpark is not present";
+        return CAR_TO_BE_UNPARKED_IS_NOT_PRESENT;
     }
 }
